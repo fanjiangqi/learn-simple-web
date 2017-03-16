e600ad9a910a5be99a5de87f055c54b2efd4a550
fanjiangqi <fanjiangqi@gmail.com>
2017-03-16 22:53:40 +0800

    3.16号 第三天mybatis加首页

--- src/main/java/com/fanjiangqi/controller/IndexController.java
+++ src/main/java/com/fanjiangqi/controller/IndexController.java
@@ -2,9 +2,11 @@ package com.fanjiangqi.controller;
 
 import com.fanjiangqi.model.User;
 import org.springframework.http.HttpRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
@@ -81,5 +83,26 @@ public class IndexController {
         response.addCookie(new Cookie(key, value));
         response.addHeader(key, value);
         return "NowCoderId From Cookie:" + nowcodeId;
+    }
+    @RequestMapping("/redirect/{code}")
+    public RedirectView redirect(@PathVariable("code") int code){
+        RedirectView red = new RedirectView("/", true);
+        if (code == 301){
+            red.setStatusCode(HttpStatus.MOVED_PERMANENTLY);
+        }
+        return  red;
+    }
+    @RequestMapping(value = {"admin"})
+    @ResponseBody
+    public String admin(@RequestParam(value = "key",required = false) String key){
+        if ("admin".equals(key)){
+            return "hello admin";
+        }
+        else throw new IllegalArgumentException("key 错误");
+    }
+    @ExceptionHandler()
+    @ResponseBody
+    public String error(Exception e){
+        return "error:" + e.getMessage();
     }
 }
